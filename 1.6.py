b04d2a682718a11b889ef1e6f027edb511aee16c
1325983698 <77277163+1325983698@users.noreply.github.com>
2021-01-11 22:26:21 +0800

    字符串、格式化输出、转义字符

--- 1.6.py
+++ 1.6.py
@@ -1,2 +1,42 @@
-print("hello world")
-print (2+3)
+# 字符串
+"""
+a = "Apple"
+b = 'banana'
+c = '''I have 
+a dream'''
+print (a)
+print (b)
+print (c)
+"""
+
+# 格式化输出
+"""
+name = "LXY"
+age = 18
+print(f"I am {name}") 
+print(f"I am {age} years old")
+"""
+
+# 转义字符
+"""
+print("apple")
+print("\napple")
+print("\tapple")
+"""
+
+# 比较运算
+"""
+a = 16
+b = 18
+print(a < b)
+print(a > b)
+"""
+# 逻辑运算
+a = True
+b = False
+print(a and b)
+print(a or b)
+#and是“并且”，交集；or是“或者”，并集
+#可将True想做1（有），False想做0（无）
+#
+
